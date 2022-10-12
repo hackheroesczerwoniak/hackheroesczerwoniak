a5d2a4d8c7af9506b6dec6269d941bf395c0f11a
--list <hackheroesczerwoniak@gmail.com>
2022-10-12 16:36:23 +0200

    Dark bg on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,7 @@ export default function Home() {
     }
 
     if (currentUser) {
-        return <div className={'h-full w-full flex items-center flex-col box-border pt-[100px]'}>
+        return <div className={'bg-[#1e1e1e] h-full w-full flex items-center flex-col box-border pt-[100px]'}>
             <img src={'https://media.tenor.com/cIUjlvgnFRgAAAAM/dog-snoop.gif'} width={200} alt={'dog'}/>
             <div className={'text-3xl text-white font-bold font-sans mt-[20px]'}>Cześć</div>
             <div className={'text-1xl text-white font-bold font-sans mt-[20px]'}>{currentUser.email}</div>
