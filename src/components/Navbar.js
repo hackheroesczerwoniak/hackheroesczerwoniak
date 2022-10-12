7923b8642bf8d30ebc7feda443c2895a9cd1b63f
--list <hackheroesczerwoniak@gmail.com>
2022-10-12 17:17:30 +0200

    Fix not centered login and registration buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ export default function Navbar() {
             <div>HackHeroes</div>
         </Link>
 
-        <div>
+        <div className={"flex flex-row items-center justify-center"}>
             <div className="flex items-center justify-center gap-4 text-white-50">
                 {currentUserEmail}
                 {logInButton}
